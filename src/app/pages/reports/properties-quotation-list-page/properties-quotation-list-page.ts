2380f511732d3f9721c1c06cc082ca924bfe6b75
Jorge C <joalcasi@gmail.com>
2025-09-22 10:52:26 -0500

    Cambios en select range

--- src/app/pages/reports/properties-quotation-list-page/properties-quotation-list-page.ts
+++ src/app/pages/reports/properties-quotation-list-page/properties-quotation-list-page.ts
@@ -6,10 +6,12 @@ import { Router } from '@angular/router';
 import { MatDatepickerModule } from '@angular/material/datepicker';
 import { MatInputModule } from '@angular/material/input';
 import { provideNativeDateAdapter } from '@angular/material/core';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { JsonPipe } from '@angular/common';
 
 @Component({
   selector: 'app-properties-quotation-list-page',
-  imports: [MenuComponent, LoaderComponent, ProjectSelectorComponent,MatInputModule, MatDatepickerModule],
+  imports: [MenuComponent, LoaderComponent, ProjectSelectorComponent,MatInputModule, MatDatepickerModule, ReactiveFormsModule, JsonPipe],
   templateUrl: './properties-quotation-list-page.html',
   styleUrls: ['./properties-quotation-list-page.css', '../../../../styles/reports.css', '../../../../styles/forms.css'],
     providers: [provideNativeDateAdapter()],
@@ -17,7 +19,20 @@ import { provideNativeDateAdapter } from '@angular/material/core';
 })
 export class PropertiesQuotationListPage {
 
+  readonly range = new FormGroup({
+    start: new FormControl<Date | null>(null),
+    end: new FormControl<Date | null>(null),
+  });
+
   constructor(private router: Router) {
+    let today = new Date().toLocaleDateString('en-CA'); // Formato YYYY-MM-DD
+    let startDate = new Date(today + ' 00:00:00').toISOString();
+    let endDate = new Date(today + ' 23:59:59').toISOString();
+
+
+    this.range.setValue({start: new Date(startDate), end: new Date(endDate)});
+    console.log('Initial range:', this.range.value);
+    
 
   }
 
