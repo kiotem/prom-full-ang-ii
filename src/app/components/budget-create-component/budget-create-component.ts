c39c47faea1133571711a4e54505948c9554c6fb
Jorge C <joalcasi@gmail.com>
2025-10-17 23:15:13 -0500

    Cambios en budget4

--- src/app/components/budget-create-component/budget-create-component.ts
+++ src/app/components/budget-create-component/budget-create-component.ts
@@ -1,9 +1,13 @@
 import { Component } from '@angular/core';
 import { PropertiesQuotationService } from '../../services/properties-quotation-service';
+import { PropertyQuoteFormComponent } from '../property-quote-form-component/property-quote-form-component';
+import { PropertyCardComponent } from '../property-card-component/property-card-component';
+import { PropertyQuoteCardComponent } from '../property-quote-card-component/property-quote-card-component';
+import { DatePipe, DecimalPipe } from '@angular/common';
 
 @Component({
   selector: 'app-budget-create-component',
-  imports: [],
+  imports: [PropertyQuoteFormComponent, PropertyQuoteCardComponent, DatePipe, DecimalPipe],
   templateUrl: './budget-create-component.html',
   styleUrls: ['./budget-create-component.css', '../../../styles/reports.css']
 })
