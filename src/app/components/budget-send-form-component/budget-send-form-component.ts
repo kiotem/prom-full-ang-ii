f8d23ec5322ea7153f716b3e900e516d2683b2ab
Jorge C <joalcasi@gmail.com>
2025-10-22 08:26:07 -0500

    Cambios en budget send form 2

--- src/app/components/budget-send-form-component/budget-send-form-component.ts
+++ src/app/components/budget-send-form-component/budget-send-form-component.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { MatIconModule } from '@angular/material/icon';
+import { MatStepperModule } from '@angular/material/stepper';
 
 @Component({
   selector: 'app-budget-send-form-component',
-  imports: [],
+  imports: [MatStepperModule, MatIconModule],
   templateUrl: './budget-send-form-component.html',
   styleUrl: './budget-send-form-component.css'
 })
