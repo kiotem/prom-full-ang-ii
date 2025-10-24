df674067e824383b8570781e5eb19ca14dad281a
Jorge C <joalcasi@gmail.com>
2025-10-24 15:30:07 -0500

    Avances cliente create 1

---
+++ src/app/components/client-create-component/client-create-component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-client-create-component',
+  imports: [],
+  templateUrl: './client-create-component.html',
+  styleUrl: './client-create-component.css'
+})
+export class ClientCreateComponent {
+
+}
